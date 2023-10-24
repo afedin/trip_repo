eb99c1e02b4b72cc7b237984e0a0de78e8fdbb2f
afedin <afedin.gis2014@gmail.com>
2023-10-24 15:45:33 +0300

    add robot.txt

--- mapapp/urls.py
+++ mapapp/urls.py
@@ -1,6 +1,15 @@
-from django.urls import path
-from . import views
+# urls.py
+
+from django.urls import path, re_path
+from django.views.static import serve
+from django.conf import settings
+from . import views  # Импорт ваших представлений
 
 urlpatterns = [
     path('', views.map, name='map'),
+    re_path(r'^robots.txt$', serve, {
+        'path': 'robots.txt',
+        'document_root': settings.STATIC_ROOT,
+    }),
 ]
+
