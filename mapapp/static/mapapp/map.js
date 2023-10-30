a72e8df5c536f7245e0e8f4ff02a8527f42e3ab6
afedin <afedin.gis2014@gmail.com>
2023-10-30 20:38:11 +0300

    add Asynchronous loading

--- mapapp/static/mapapp/map.js
+++ mapapp/static/mapapp/map.js
@@ -8,71 +8,69 @@ L.control.zoom({
     position: 'bottomright'
 }).addTo(map);
 
-let geoLayer;
-
-fetch('/static/data/countries.geojson')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
+// Инициализация гео-слоя
+let geoLayer = L.geoJSON(null, {
+    style: (feature) => {
+        const legend = feature.properties.legend;
+        // ваша логика стилизации...
+        switch (legend) {
+            case 'Россия':
+                return {color: '#858482', fillColor: '#abaff0', fillOpacity: 0.45, weight: 1, opacity: 0.45};
+            case 'Паспорт РФ':
+                return {color: '#858482', fillColor: '#0044cd', fillOpacity: 0.45, weight: 1, opacity: 0.45};
+            case 'Без визы':
+                return {color: '#858482', fillColor: '#a5ff6c', fillOpacity: 0.45, weight: 1, opacity: 0.45};
+            case 'Электронная виза':
+                return {color: '#858482', fillColor: '#c3d790', fillOpacity: 0.45, weight: 1, opacity: 0.45};
+            case 'Виза по прибытии':
+                return {color: '#858482', fillColor: '#7dae61', fillOpacity: 0.45, weight: 1, opacity: 0.45};
+            case 'Требуется виза':
+                return {color: '#858482', fillColor: '#eab25e', fillOpacity: 0.35, weight: 1, opacity: 0.45};
+            case 'Отказано во въезде':
+                return {color: '#858482', fillColor: '#d7191c', fillOpacity: 0.45, weight: 1, opacity: 0.45};    
+            case 'Неизвестно':
+                return {color: '#858482', fillColor: '#a2c0cc', fillOpacity: 0.055, weight: 1, opacity: 0.21};
+            default:
+                return {color: '#858482', fillColor: '#2b83ba', fillOpacity: 0.45, weight: 1, opacity: 0.45};
         }
-        return response.json();
-    })
-    .then(data => {
-        geoLayer = L.geoJSON(data, {
-            style: (feature) => {
-                const legend = feature.properties.legend;
-                switch (legend) {
-                    case 'Россия':
-                        return {color: '#858482', fillColor: '#abaff0', fillOpacity: 0.45, weight: 1, opacity: 0.45};
-                    case 'Паспорт РФ':
-                        return {color: '#858482', fillColor: '#0044cd', fillOpacity: 0.45, weight: 1, opacity: 0.45};
-                    case 'Без визы':
-                        return {color: '#858482', fillColor: '#a5ff6c', fillOpacity: 0.45, weight: 1, opacity: 0.45};
-                    case 'Электронная виза':
-                        return {color: '#858482', fillColor: '#c3d790', fillOpacity: 0.45, weight: 1, opacity: 0.45};
-                    case 'Виза по прибытии':
-                        return {color: '#858482', fillColor: '#7dae61', fillOpacity: 0.45, weight: 1, opacity: 0.45};
-                    case 'Требуется виза':
-                        return {color: '#858482', fillColor: '#eab25e', fillOpacity: 0.35, weight: 1, opacity: 0.45};
-                    case 'Отказано во въезде':
-                        return {color: '#858482', fillColor: '#d7191c', fillOpacity: 0.45, weight: 1, opacity: 0.45};    
-                    case 'Неизвестно':
-                        return {color: '#858482', fillColor: '#a2c0cc', fillOpacity: 0.055, weight: 1, opacity: 0.21};
-                    default:
-                        return {color: '#858482', fillColor: '#2b83ba', fillOpacity: 0.45, weight: 1, opacity: 0.45};
-                }
-              
-            },
-            // onEachFeature: function(feature, layer) {
-            //     if (feature.properties && feature.properties.NAME_RU) {
-            //         layer.bindTooltip(feature.properties.NAME_RU, {
-            //             permanent: false,
-            //             direction: 'auto',
-            //             className: 'country-label',
-            //             followCursor: true
-            //         });
-            //     }
-            // }
-        }).addTo(map);
+      
+    },
+        
+    }).addTo(map);
 
-        geoLayer.on('mouseover', function(e) {
-            const tooltip = L.tooltip({
-                permanent: false,
-                direction: 'center',
-                className: 'country-label'
-            })
-            .setContent(e.layer.feature.properties.NAME_RU)
-            .setLatLng(e.latlng)
-            .addTo(map);
-            e.layer._tempTooltip = tooltip;
-        })
-        .on('mouseout', function(e) {
-            map.removeLayer(e.layer._tempTooltip);
-        });
+
+// Диапазон номеров файлов
+const fileNumbers = Array.from({length: 262}, (_, i) => i + 1);
+
+fileNumbers.forEach(number => {
+    // Путь к файлу в статических ресурсах Django
+    const filePath = `/static/data/${number}.geojson`;
+    
+    fetch(filePath)
+    .then(response => response.json())
+    .then(data => {
+        // Добавление GeoJSON к текущему слою
+        geoLayer.addData(data);
     })
     .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
+        console.error(`Error loading ${filePath}: ${error}`);
     });
+});
+
+geoLayer.on('mouseover', function(e) {
+    const tooltip = L.tooltip({
+        permanent: false,
+        direction: 'center',
+        className: 'country-label'
+    })
+    .setContent(e.layer.feature.properties.NAME_RU)
+    .setLatLng(e.latlng)
+    .addTo(map);
+    e.layer._tempTooltip = tooltip;
+})
+.on('mouseout', function(e) {
+    map.removeLayer(e.layer._tempTooltip);
+});
 
 map.on('zoomend', function() {
     const zoomLevel = map.getZoom();
@@ -101,3 +99,6 @@ legend.onAdd = function(map) {
 };
 
 legend.addTo(map);
+
+
+
